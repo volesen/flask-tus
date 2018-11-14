4cf87ff403183c9a2f2c1cf8ccc835c302965237
Vincent Olesen <vincolesen@gmail.com>
2018-11-14 20:13:40 +0100

    Fixed file-extensions and ascii decoding

--- flask_tus/helpers.py
+++ flask_tus/helpers.py
@@ -2,12 +2,12 @@ import base64
 
 
 def get_extension(filename, depth=1):
-    return filename.split(".")[1:]
+    return '.'.join(filename.split(".")[1:])
 
 
 def handle_metadata(upload_metadata):
     metadata = {}
     for kv in upload_metadata.split(','):
         key, value = kv.split(' ')
-        metadata[key] = base64.b64decode(value).encode('ascii')
+        metadata[key] = base64.b64decode(value).decode('ascii')
     return metadata
