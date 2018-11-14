a5726f422f7283072ff383bcdc97c0b47c800426
ldynia <ludd@cbs.dtu.dk>
2018-11-14 15:11:20 +0100

    addinx ext module to setup.py

--- flask_tus/setup.py
+++ flask_tus/setup.py
@@ -2,19 +2,17 @@ import sys
 from setuptools import setup
 
 setup(
-    name = "flask-tus",        # what you want to call the archive/egg
-    version = "0.1",
-    packages=["flask_tus"],    # top-level python modules you can import like
-                                #   'import foo'
-    dependency_links = [],      # custom links to a specific project
+    name="flask-tus",
+    version="0.1",
+    packages=["flask_tus", "flask_tus.ext"],
+    dependency_links=[],
     install_requires=[],
-    extras_require={},      # optional features that other packages can require
-                            #   like 'helloworld[foo]'
-    package_data = {},
+    extras_require={},
+    package_data={},
     author="Vincent Olesen",
-    author_email = "private@email.com",
-    description = "Flask server for tus protocol",
-    license = "MIT",
-    keywords= " documentation tutorial",
-    url = "http://github.com/volesen/flask-tus",
+    author_email="private@email.com",
+    description="Flask server for tus protocol",
+    license="MIT",
+    keywords=" documentation tutorial",
+    url="http://github.com/volesen/flask-tus",
 )
