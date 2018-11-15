88c68c7b8e127c7878bf524884f0fa1b5c766bc6
ldynia <ludd@cbs.dtu.dk>
2018-11-15 11:24:32 +0100

    commenting flask-mongoengine code in demo/app.py

--- demo/app.py
+++ demo/app.py
@@ -1,22 +1,24 @@
 import os
 
 from flask import Flask, render_template
-from flask_tus.main import FlaskTus
-from flask_mongoengine import MongoEngine
+from flask_tus.app import FlaskTus
+
 
 app = Flask(__name__)
-app.config['TUS_UPLOAD_DIR'] = os.getcwd() + '/storage/uploads'
-app.config['TUS_UPLOAD_URL'] = '/files/'
-app.config['TUS_MAX_SIZE'] = 2 ** 32  # 4GB
-app.config['MONGODB_SETTINGS'] = {
-    'db': DB_NAME,
-    'host': DB_HOST,
-    'port': DB_PORT,
-}
 
+# from flask_mongoengine import MongoEngine
+# app.config['TUS_UPLOAD_DIR'] = os.getcwd() + '/storage/uploads'
+# app.config['TUS_UPLOAD_URL'] = '/files/'
+# app.config['TUS_MAX_SIZE'] = 2 ** 32  # 4GB
+# app.config['MONGODB_SETTINGS'] = {
+#     'db': DB_NAME,
+#     'host': DB_HOST,
+#     'port': DB_PORT,
+# }
+# mongodb = Mpngoengine(app)
+# flask_tus = FlaskTus(app, mongodb)
 
-mongodb = Mpngoengine(app)
-flask_tus = FlaskTus(app, mongodb)
+flask_tus = FlaskTus(app)
 
 
 @app.route('/')
