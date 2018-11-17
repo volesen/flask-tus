cdecfe366266ea4345e1686b9dfda7b0dd56d7de
Vincent Olesen <vincolesen@gmail.com>
2018-11-17 19:38:10 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
-import sys
 from setuptools import setup
+
+with open('README.md') as readme:
+    long_description = readme.read()
 
 setup(
     name="flask-tus",
     version="0.1",
-    packages=["flask_tus", "flask_tus.models"],
+    packages=["flask_tus"],
     dependency_links=[],
-    install_requires=[],
+    install_requires=['Flask'],
     extras_require={},
     package_data={},
     author="Vincent Olesen",
     author_email="private@email.com",
-    description="Flask server for tus protocol",
+    description="Tus protocol 1.0.0 Flask server implementation",
+    long_description=long_description,
     license="MIT",
     keywords=" documentation tutorial",
     url="http://github.com/volesen/flask-tus",
