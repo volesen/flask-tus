5587f577497473b3bc31e6624058949ee3c7ed37
Vincent Olesen <vincolesen@gmail.com>
2018-12-03 16:04:58 +0100

    Adding model tests

--- tests/config.py
+++ tests/config.py
@@ -1,6 +1,9 @@
 from datetime import timedelta
 from tempfile import mkdtemp
 
+from flask_tus.models.memory_upload import MemoryUpload
+from flask_tus.models.mongoengine_upload import MongoengineUpload
+
 
 class BaseTestConfig:
     TESTING = True
@@ -11,4 +14,8 @@ class BaseTestConfig:
 
 
 class MemoryUploadConfig(BaseTestConfig):
-    pass
+    UPLOAD_MODEL = MemoryUpload
+
+
+class MongoUploadConfig(BaseTestConfig):
+    UPLOAD_MODEL = MongoengineUpload
--- tests/conftest.py
+++ tests/conftest.py
@@ -12,13 +12,14 @@ def create_app(config):
     app.config.from_object(config)
     flask_tus = FlaskTus()
     mongoengine.connect('mongoenginetest', host='mongomock://localhost')
-    flask_tus.init_app(app, model=app.config['MODEL'])
+    flask_tus.init_app(app, model=app.config['UPLOAD_MODEL'])
 
     return app, flask_tus
 
 
 @pytest.fixture(scope='class')
 def class_client(request):
+    # TODO Run tests on all models
     request.cls.app, request.cls.flask_tus = create_app(MongoUploadConfig)
     request.cls.client = request.cls.app.test_client()
     yield
