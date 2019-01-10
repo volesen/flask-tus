9e10e5d0a0429ed142bc3c648e19865d234aa39f
volesen <vincolesen@gmail.com>
2019-01-10 14:33:00 +0100

    Added tests for deletion callbacks

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -11,6 +11,8 @@ class TestCallbacks(object):
     post_saves = 0
     created = False
     completed = False
+    pre_delete = False
+    post_delete = False
 
     @staticmethod
     def __read_chunks(file, chunk_size):
@@ -28,12 +30,20 @@ class TestCallbacks(object):
     def _on_complete(self):
         self.completed = True
 
+    def _pre_delete(self):
+        self.pre_delete = True
+
+    def _post_delete(self):
+        self.post_delete = True
+
     def test_callbacks(self):
         # Inject callbacks and mock upload
         self.app.flask_tus.pre_save = self._pre_save
         self.app.flask_tus.post_save = self._post_save
         self.app.flask_tus.on_create = self._on_create
         self.app.flask_tus.on_complete = self._on_complete
+        self.app.flask_tus.pre_delete = self._pre_delete
+        self.app.flask_tus.post_delete = self._post_delete
 
         with open(TEST_FILE, 'rb') as file:
             file_length = len(file.read())
@@ -49,6 +59,7 @@ class TestCallbacks(object):
 
             assert self.created
             resource_url = post_response.headers['Location']
+            resource_id = resource_url.split('/')[-1]
 
             offset = 0
             chunks = 0
@@ -67,3 +78,10 @@ class TestCallbacks(object):
             assert self.pre_saves == chunks
             assert self.post_saves == chunks
             assert self.completed
+        
+        # Assert callback side-effects, assuming successful deletion
+        with self.app.app_context():
+            self.flask_tus.model.get(resource_id).delete()
+
+        assert self.pre_delete
+        assert self.post_delete
