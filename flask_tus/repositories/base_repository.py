381844e285d5797103fd18b58d575ddf525ea1f3
volesen <vincolesen@gmail.com>
2019-01-30 13:45:32 +0100

    Refactored repository interface

--- flask_tus/repositories/base_repository.py
+++ flask_tus/repositories/base_repository.py
@@ -1,34 +1,36 @@
 class BaseRepository(object):
     """This class is an interface"""
     model = None
+    session = None
 
-    def __init__(self, model):
+    def __init__(self, model, session):
         self.model = model
+        self.session = session
 
     def instantiate(self, **kwargs):
         """Crate instance of a model"""
-        pass
+        raise NotImplementedError
 
     def create(self, **kwargs):
         """Crate instance and save model"""
-        pass
+        raise NotImplementedError
 
     def save(self, instance, boolean=True):
         """Save instance of model"""
-        pass
+        raise NotImplementedError
 
     def update(self, instance, **kwargs):
         """Update already saved instance of model"""
-        pass
+        raise NotImplementedError
 
     def find_by(self, **kwargs):
         """Find instances by **kwargs"""
-        pass
+        raise NotImplementedError
 
     def find_all(self, id):
         """Find all instances"""
-        pass
+        raise NotImplementedError
 
     def find_by_id(self, id):
         """Find instance by model id"""
-        pass
+        raise NotImplementedError
