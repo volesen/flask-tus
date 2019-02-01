dce4363c1e7250561301554b20194c04f3e26e51
volesen <vincolesen@gmail.com>
2019-02-01 15:43:27 +0100

    Refactored and updated setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+import setuptools
 
 with open('README.md') as readme:
     long_description = readme.read()
 
-setup(
+with open('requirements.txt') as requirements:
+    install_requires = [x.strip() for x in requirements.read().split(
+        '\n') if not x.startswith('#')]
+
+setuptools.setup(
     name="flask-tus",
     version="0.1",
-    packages=find_packages(),
-    dependency_links=[],
-    install_requires=['Flask>=1.0.2', 'mongoengine>=0.16.2'],
-    test_requires=['pytest>=4.0.0'],
-    extras_require={},
-    package_data={},
     author="Vincent Olesen",
     author_email="private@email.com",
     description="Tus protocol 1.0.0 server implementation for Flask",
     long_description=long_description,
-    license="MIT",
-    keywords=" documentation tutorial",
+    long_description_content_type="text/markdown",
     url="http://github.com/volesen/flask-tus",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires, 
+    tests_require=['pytest>=4.0.0'],
+    zip_safe=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     entry_points={
         'flask.commands': [
             'tus=flask_tus.manage.commands:cli'
