4cf0f8a81ec53423c8a75e07caea9d70c9cb9a11
volesen <vincolesen@gmail.com>
2019-02-01 13:08:23 +0100

    Refactored Docker and docker-compse

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,7 @@ COPY requirements.txt /
 RUN pip install -r /requirements.txt
 
 COPY . /app
+
+RUN python setup.py install
+
+CMD ["python", "example/app.py"]
--- scripts/startup_app.sh
+++
@@ -1,9 +0,0 @@
-# HANDLE PIP
-pip install --upgrade pip
-pip install -r requirements.txt
-
-python setup.py install
-
-# START PYTHON
-python example/app.py
-# gunicorn example/app:app -b 0.0.0.0:80
