8b760c23909ef2e8f770748f946fb8bc40d3e346
ldynia <l_dynowski@yahoo.com>
2020-06-03 13:53:55 +0200

    adding exist_ok=True to makedirs()

--- flask_tus/repositories/mongoengine_repository.py
+++ flask_tus/repositories/mongoengine_repository.py
@@ -19,7 +19,7 @@ class MongoengineRepository(BaseRepository):
 
     def create(self, length, metadata, **kwargs):
         upload_dir = time.strftime(current_app.config['TUS_UPLOAD_DIR'])
-        os.makedirs(upload_dir)
+        os.makedirs(upload_dir, exist_ok=True)
         path = os.path.join(upload_dir, str(uuid.uuid4()))
 
         if metadata and metadata.get('filename'):
